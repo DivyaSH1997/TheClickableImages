3eee5ad79fdeb76029f1fdaa29d3c7e1211623b9
DivyaSH1997 <div.12213443@gmail.com>
2022-12-31 14:10:02 +0530

    ClickImages

--- app/src/main/java/com/example/clickableimages/MainActivity.java
+++ app/src/main/java/com/example/clickableimages/MainActivity.java
@@ -2,11 +2,8 @@ package com.example.clickableimages;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -16,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnGoa, btnManali, btnShillong, btnJodhpur;
     ImageView imageDisplay;
-    String str;
+
 
     int picsGoa[] = {R.drawable.gone, R.drawable.gtwo, R.drawable.gthree, R.drawable.gfour};
     int picsManali[] = {R.drawable.mone, R.drawable.mtwo, R.drawable.mthree, R.drawable.mfour};
@@ -29,12 +26,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initViews();
+        initListeners();
+
+    }
+    public void initViews()
+    {
         btnGoa = findViewById(R.id.btnGoa);
         btnManali = findViewById(R.id.btnManali);
         btnJodhpur = findViewById(R.id.btnJodhpur);
         btnShillong = findViewById(R.id.btnShillong);
         imageDisplay = findViewById(R.id.imageDisplay);
-
+    }
+    public void initListeners()
+    {
         btnGoa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -80,7 +85,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
     }
 
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -88,8 +92,8 @@ public class MainActivity extends AppCompatActivity {
 
         Bundle bundle = data.getExtras();
 
-        int x = bundle.getInt("id");
-        imageDisplay.setImageResource(x);
+        int imageid = bundle.getInt("id");
+        imageDisplay.setImageResource(imageid);
 
     }
 
--- app/src/main/java/com/example/clickableimages/SecondActivity.java
+++ app/src/main/java/com/example/clickableimages/SecondActivity.java
@@ -1,15 +1,10 @@
 package com.example.clickableimages;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.media.Image;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 
-import androidx.annotation.Nullable;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
@@ -23,13 +18,20 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        imgA = findViewById(R.id.imgAA);
-        imgB = findViewById(R.id.imgBB);
-        imgC = findViewById(R.id.imgCC);
-        imgD = findViewById(R.id.imgDD);
+        initViews();
+        getDetails();
+        initListeners();
 
-
-
+    }
+    public void initViews()
+    {
+        imgA = findViewById(R.id.imgA);
+        imgB = findViewById(R.id.imgB);
+        imgC = findViewById(R.id.imgC);
+        imgD = findViewById(R.id.imgD);
+    }
+    public void getDetails()
+    {
         Intent forward = getIntent();
         Bundle bundle = forward.getExtras();
         for (String key : bundle.keySet()) {
@@ -63,10 +65,8 @@ public class SecondActivity extends AppCompatActivity {
             imgC.setImageResource(imgArray[2]);
             imgD.setImageResource(imgArray[3]);
         }
-        clicker();
-
     }
-    public void clicker()
+    public void initListeners()
     {
 
         imgA.setOnClickListener(new View.OnClickListener() {
@@ -75,8 +75,6 @@ public class SecondActivity extends AppCompatActivity {
 
                 Intent backward=new Intent();
                 backward.putExtra("id",imgArray[0]);
-
-                //backward.putExtra("ImgA",imgA.setImageResource(R.id.imgAA));
 
                 setResult(1,backward);
                 finish();
@@ -87,6 +85,7 @@ public class SecondActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent backward=new Intent();
                 backward.putExtra("id",imgArray[1]);
+
                 setResult(1,backward);
                 finish();
             }
@@ -98,6 +97,7 @@ public class SecondActivity extends AppCompatActivity {
 
                 Intent backward=new Intent();
                 backward.putExtra("id",imgArray[2]);
+
                 setResult(1,backward);
                 finish();
             }
@@ -109,6 +109,7 @@ public class SecondActivity extends AppCompatActivity {
 
                 Intent backward = new Intent();
                 backward.putExtra("id",imgArray[3]);
+
                 setResult(1, backward);
                 finish();
             }
